e4e7bacc2c5cde498b746cf61c6c68860fdfdb98
Andrei Marukhin <ax2mx@ya.ru>
2021-08-31 00:38:43 +0300

    Add theme toggler

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,15 @@
 <script>
 	import 'virtual:windi.css';
-
-	// if you want to enable windi devtools
+	// Enable windi devtools
 	import { browser } from '$app/env';
-	if (browser) import('virtual:windi-devtools');
+	if (browser) {
+		import('virtual:windi-devtools');
+	}
+
+	import ThemeToggle from '$lib/ThemeToggle.svelte';
 </script>
 
-<div class="p-8 mx-auto w-full">
+<div class="min-h-screen p-8 mx-auto w-full text-gray-900 bg-white dark:(bg-gray-900 text-white)">
+	<ThemeToggle/>
 	<slot />
 </div>
